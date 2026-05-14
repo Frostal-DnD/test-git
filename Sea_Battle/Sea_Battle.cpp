cf4c26354d41e0e589d5368f19ab1847db5daef7
Frostal-Dnd <andreilesnik.2021@gmail.com>
2026-05-14 11:38:11 +0300

    test2

---
+++ Sea_Battle/Sea_Battle.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <Windows.h>
+#include <conio.h>
+#include <iomanip>
+#include <string.h>
+
+using namespace std;
+
+//Graphic Tools
+void Col(int bg, int txt) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, (WORD)((bg << 4) | txt));
+}
+
+void setcur(int x, int y) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(hConsole, coord);
+}
+
+
+
+int main()
+{
+   cout << "Hello World!\n";
+}
